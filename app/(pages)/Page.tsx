6d79e35ebcb6e0d438c5196a671b5ea99a7ee842
Leonam1212 <leonam_5000@hotmail.com>
2025-07-06 15:41:12 -0300

    Save Project

---
+++ app/(pages)/Page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+export function Page() {
+  return (
+    <div className="shadow-md bg-red-200 h-10 p-4 rounded-lg">
+      <h1>Page</h1>
+      <p>This is a page component.</p>
+    </div>
+  )
+}
